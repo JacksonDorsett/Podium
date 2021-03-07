d8f7efef6ede1e21c0dbef27c4cde357e8216a88
jackson <dorsettj@gmail.com>
2021-03-06 19:54:05 -0800

    Added threading to servermanager

--- VPodium/Assets/NetPeer/ServerManager.cs
+++ VPodium/Assets/NetPeer/ServerManager.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 using System.Net;
 using UnityEngine;
 using System.Net.Sockets;
+using System.Threading;
+
 namespace Assets.NetPeer
 {
     class ServerManager : MonoBehaviour
     {
-
+        private int port = 8083;
+        private IPEndPoint ipEndPoint;
         public string str;
         public IPAddress myIp;
         private Socket serverListener;
@@ -21,10 +24,9 @@ namespace Assets.NetPeer
         {
             connectedClients = new List<Socket>();
             clientSocket = default(Socket);
-            connectedClientCount = 0;
-            serverListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            str = GetIPString();
-            myIp = GetIP();
+            Bind();
+            Thread clientThread = new Thread(new ThreadStart(() =>
+           this.Listen()));
 
         }
 
@@ -35,11 +37,15 @@ namespace Assets.NetPeer
 
         private void Listen()
         {
+            Debug.Log("Starting Server");
             while (true)
             {
                 clientSocket = serverListener.Accept();
                 connectedClientCount++;
                 connectedClients.Add(clientSocket);
+
+                Thread clientThread = new Thread(new ThreadStart( () =>
+                    this.ListenUser(clientSocket)));
             }
         }
 
@@ -50,7 +56,33 @@ namespace Assets.NetPeer
                 return myIp;
             }
         }
+        private void ListenUser(Socket client)
+        {
+            Debug.Log($"Client connected with ip: {client.RemoteEndPoint.ToString()}")
+            while (client.Connected)
+            {
+                byte[] msg = new byte[1024];
+                int size = client.Receive(msg);
+                Console.WriteLine("Client>> " +
+                System.Text.Encoding.ASCII.GetString(msg, 0, size));
 
+                // rebroadcast
+                foreach (Socket clientSocket in connectedClients)
+                {
+                    client.Send(msg, 0, size, SocketFlags.None);
+                }
+            }
+
+            connectedClientCount--;
+        }
+        private void Bind()
+        {
+            str = GetIPString();
+            myIp = GetIP();
+            connectedClientCount = 0;
+            serverListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ipEndPoint = new IPEndPoint(IPAddress, port);
+        }
         private string GetIPString()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
