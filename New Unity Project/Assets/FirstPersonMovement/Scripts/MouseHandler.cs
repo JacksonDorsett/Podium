10a4949b97b6b5d0551cb905eff967bef0276745
Freeman-gif <freemanchen115@gmail.com>
2021-03-07 02:41:26 -0800

    updated movement to handle mouse and direction

--- New Unity Project/Assets/FirstPersonMovement/Scripts/MouseHandler.cs
+++ New Unity Project/Assets/FirstPersonMovement/Scripts/MouseHandler.cs
@@ -1,37 +1,81 @@
 ﻿using UnityEngine;
-
+[RequireComponent(typeof(CharacterController))]
 public class MouseHandler : MonoBehaviour
 {
     // horizontal rotation speed
-    public float horizontalSpeed = 1f;
+    // public float horizontalSpeed = 1f;
     // vertical rotation speed
-    public float verticalSpeed = 1f;
-    public float xRotation = 0.0f;
-    private float yRotation = 0.0f;
-    private Camera cam;
+    //public float verticalSpeed = 1f;
+    // public float xRotation = 0.0f;
+    //private float yRotation = 0.0f;
+    //private Camera cam;
+    public float walkingSpeed = 5.5f;
+    public float runningSpeed = 8.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera cam;
+    public float lookSpeed = 2.0f;
+    public float lookXlimit = 45.0f;
 
-    public Vector3 HorizontalDirection
-    {
-        get
-        {
-            Vector3 direction = new Vector3(Mathf.Sin(yRotation), 0, Mathf.Cos(yRotation)).normalized;
-            return direction;
-        }
-    }
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
+
+
     void Start()
     {
-        cam = Camera.main;
+        characterController = GetComponent<CharacterController>();
+        //lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+        // We are grounded, so recalcultae move direction based on 2  axaes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        //Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        if(Input.GetButton("Jump") )//&& characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        //xRotation = Mathf.Clamp(xRotation, 0, 90);
+        if(!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
 
-        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXlimit, lookXlimit);
+            cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+
+
+
+        }
     }
+
+
+
+
+
 }
+ 
