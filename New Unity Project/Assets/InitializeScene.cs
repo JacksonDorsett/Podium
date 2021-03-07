fa06caed9853cab738f0843931fb2cbc5974999d
jackson <dorsettj@gmail.com>
2021-03-07 03:14:20 -0800

    fixed mouse bug

---
+++ New Unity Project/Assets/InitializeScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets;
+public class InitializeScene : MonoBehaviour
+{
+    private GameObject server;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if(GlobalControl.Instance.isHost)
+        {
+            InitializeHost();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeHost()
+    {
+        GameObject.Instantiate(Resources.Load("Server") as GameObject);
+    }
+}
